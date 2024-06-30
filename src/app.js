d87bb19130e993318838905973266f3fa4ae873b
Aman Agarwal <amanagarwal3092001@gmail.com>
2024-06-30 14:17:50 +0530

    add register controller and bug fixes

--- src/app.js
+++ src/app.js
@@ -13,4 +13,18 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit : "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+//routes
+
+import userRouter from "./routes/user.routes.js"
+
+
+
+
+//routes declatration
+//pehle directly idhar hi route aur controller likh rhe the par ab humne router alag jagah kr dia hain to ab .use use krke middleware ki help se lana hoga
+
+app.use("/api/v1/users",userRouter) //jaise hi koi type krega /user hum control de denge userRouter pe jo ki userRouter file pe control dega aur route pe leke jaega
+//iise hhttp://localhost:8000/api/v1/users pe hoga dfir jab userRoute pe jaega to aage / register hai ya / login us according aage ke controller work krte
+
 export { app }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import dotenv from "dotenv";
 //import mongoose from "mongoose";
 //import {DB_NAME} from "./constants"
 import connectDB from "./db/index.js";
-
+import express from "express"
 dotenv.config({
     path: './env'
 })
-
+import { app } from "./app.js";
 connectDB()
 .then(()=>{
     app.listen(process.env.PORT || 8000, ()=>{
