da334b151ee0866723fe90841654890597c712db
Aman Agarwal <amanagarwal3092001@gmail.com>
2024-07-04 20:26:40 +0530

    refreshAccessToken

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser } from "../contollers/user.controller.js";
+import { loginUser, logoutUser, registerUser, refreshAccessToken } from "../contollers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router()
@@ -22,5 +22,9 @@ router.route("/register").post(
     //secured routes
 
     router.route("/logout").post( verifyJWT, logoutUser)
+   
+    router.route("/refresh-token").post(refreshAccessToken)
+
+
 
 export default router
